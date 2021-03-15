aa2b80f9be29e5d246e85fd9f1c33cea1167aef4
gabrielsdinizbrq <gabrielsdiniz@brq.com>
2021-03-15 18:14:40 -0300

    News Slider updates

--- components/NewsMainPageContainer.js
+++ components/NewsMainPageContainer.js
@@ -2,14 +2,37 @@ import NewsNav from '../components/NewsNav';
 import NewsMainSlider from '../components/NewsMainSlider';
 import AdContainer from './AdContainer';
 import NewsCard from '../components/NewsCard';
+import styled from 'styled-components';
 
-export default function NewsMainPageContainer({ posts }) {
+const SliderContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0;
+  background: #000;
+  flex: 75%;
+  margin-right: 20px;
+  @media (max-width: 1100px) {
+    margin-right: 0;
+  }
+  @media (max-width: 768px) {
+    margin-top: 20px;
+    flex-direction: column;
+  }
+`
+
+export default function NewsMainPageContainer({ posts, sliderPosts }) {
   return (
     <main>
       <div className="container">
         <NewsNav/>
         <div className="row">
-          <NewsMainSlider/>
+          <SliderContainer>
+            {sliderPosts.map((sliderPost) => (
+                  <NewsMainSlider sliderPost={sliderPost} key={sliderPost}/>
+                )
+            )}
+          </SliderContainer>
           <AdContainer Height={480} margins={0} />
         </div>
         <div className="row">
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Header from '../components/Header';
 import NewsMainPageContainer from '../components/NewsMainPageContainer';
 import Footer from '../components/Footer';
 
-export default function Home({ posts }) {
+export default function Home({ posts, sliderPosts }) {
   return (
     <>
       <Head>
@@ -21,19 +21,22 @@ export default function Home({ posts }) {
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet" />
       </Head>
       <Header />
-      <NewsMainPageContainer posts={posts} />
+      <NewsMainPageContainer posts={posts} sliderPosts={sliderPosts} />
       <Footer />
     </>
   )
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch(`https://mubdmn-dev.crdps.xyz/wp-json/wp/v2/posts/`);
-  const posts = await res.json();
+  const resPost = await fetch(`https://mubdmn-dev.crdps.xyz/wp-json/wp/v2/posts?tags_exclude=17`);
+  const resSlider = await fetch(`https://mubdmn-dev.crdps.xyz/wp-json/wp/v2/posts?per_page=2&?tags=17`);
+  const posts = await resPost.json();
+  const sliderPosts = await resSlider.json();
   
   return {
     props: {
-      posts
+      posts,
+      sliderPosts
     }
   }
 
